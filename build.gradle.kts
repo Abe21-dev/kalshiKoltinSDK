21aa43b7cfd0528778f1a4f2c088c0912d9df6a6
abe <54418157+Abe21-dev@users.noreply.github.com>
2025-11-28 08:33:49 -0800

    updated gitignore

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,10 @@ repositories {
     mavenCentral()
 }
 
+val ktor_version: String by project
 dependencies {
+    implementation("io.ktor:ktor-client-core:${ktor_version}")
+    implementation("io.ktor:ktor-client-cio:${ktor_version}")
     testImplementation(kotlin("test"))
 }
 
