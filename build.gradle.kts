7a64ba403808c27d683c22b54463cef5c4142bd9
abe <54418157+Abe21-dev@users.noreply.github.com>
2025-11-28 08:46:32 -0800

    clean start

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,10 +9,7 @@ repositories {
     mavenCentral()
 }
 
-val ktor_version: String by project
 dependencies {
-    implementation("io.ktor:ktor-client-core:${ktor_version}")
-    implementation("io.ktor:ktor-client-cio:${ktor_version}")
     testImplementation(kotlin("test"))
 }
 
